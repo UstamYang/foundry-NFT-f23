bd5d63f865d0b61560a22490d98315e957857c60
UstamY <haotian.y@yahooo.com>
2025-01-09 00:36:27 -0800

    finished Sec.16

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -6,6 +6,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    //errors
+    error MoodNft_CantFlipMoodIfNotOwner();
     uint256 private s_tokenCounter;
     string private s_sadSvgImageURI;
     string private s_happySvgImageURI;
@@ -16,6 +18,7 @@ contract MoodNft is ERC721 {
     }
 
     mapping(uint256 => Mood) private s_tokenIdToMood;
+    mapping(uint256 => address) private s_tokenIdToOwner;
 
     constructor(
         string memory sadSvgImageURI,
@@ -29,11 +32,33 @@ contract MoodNft is ERC721 {
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
+        s_tokenIdToOwner[s_tokenCounter] = msg.sender;
         s_tokenCounter++;
     }
 
     function _baseURI() internal pure override returns (string memory) {
         return "data:application/json;base64,";
+    }
+
+    function _isApprovedOrOwner(
+        address spender,
+        uint256 tokenId
+    ) public view returns (bool) {
+        return
+            _isApprovedOrOwner(spender, tokenId) ||
+            s_tokenIdToOwner[tokenId] == spender;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        //We only want the owner to be able to flip the mood
+        if (!_isApprovedOrOwner(msg.sender, tokenId)) {
+            revert MoodNft_CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function tokenURI(
